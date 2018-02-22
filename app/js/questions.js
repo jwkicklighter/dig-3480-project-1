07a33cc9e880f09222c86f0139363dee20612d65
Jordan Kicklighter <jwkicklighter@gmail.com>
2018-02-21 19:41:26 -0500

    Modify questions

--- app/js/questions.js
+++ app/js/questions.js
@@ -1,92 +1,92 @@
 module.exports = [
   {
-    prompt: 'Question 1',
+    prompt: 'This is the prompt for question 1',
     answers: [
-      { label: 'Answer 1', name: 'one' },
-      { label: 'Answer 2', name: 'one' },
-      { label: 'Answer 3', name: 'one' },
-      { label: 'Answer 4', name: 'one' }
+      { label: 'Answer 1', name: '1' },
+      { label: 'Answer 2', name: '1' },
+      { label: 'Answer 3', name: '1' },
+      { label: 'Answer 4', name: '1' }
     ]
   },
   {
-    prompt: 'Question 2',
+    prompt: 'This is the prompt for question 2',
     answers: [
-      { label: 'Answer 1', name: 'two' },
-      { label: 'Answer 2', name: 'two' },
-      { label: 'Answer 3', name: 'two' },
-      { label: 'Answer 4', name: 'two' }
+      { label: 'Answer 1', name: '2' },
+      { label: 'Answer 2', name: '2' },
+      { label: 'Answer 3', name: '2' },
+      { label: 'Answer 4', name: '2' }
     ]
   },
   {
-    prompt: 'Question 3',
+    prompt: 'This is the prompt for question 3',
     answers: [
-      { label: 'Answer 1', name: 'three' },
-      { label: 'Answer 2', name: 'three' },
-      { label: 'Answer 3', name: 'three' },
-      { label: 'Answer 4', name: 'three' }
+      { label: 'Answer 1', name: '3' },
+      { label: 'Answer 2', name: '3' },
+      { label: 'Answer 3', name: '3' },
+      { label: 'Answer 4', name: '3' }
     ]
   },
   {
-    prompt: 'Question 4',
+    prompt: 'This is the prompt for question 4',
     answers: [
-      { label: 'Answer 1', name: 'four' },
-      { label: 'Answer 2', name: 'four' },
-      { label: 'Answer 3', name: 'four' },
-      { label: 'Answer 4', name: 'four' }
+      { label: 'Answer 1', name: '4' },
+      { label: 'Answer 2', name: '4' },
+      { label: 'Answer 3', name: '4' },
+      { label: 'Answer 4', name: '4' }
     ]
   },
   {
-    prompt: 'Question 5',
+    prompt: 'This is the prompt for question 5',
     answers: [
-      { label: 'Answer 1', name: 'five' },
-      { label: 'Answer 2', name: 'five' },
-      { label: 'Answer 3', name: 'five' },
-      { label: 'Answer 4', name: 'five' }
+      { label: 'Answer 1', name: '5' },
+      { label: 'Answer 2', name: '5' },
+      { label: 'Answer 3', name: '5' },
+      { label: 'Answer 4', name: '5' }
     ]
   },
   {
-    prompt: 'Question 6',
+    prompt: 'This is the prompt for question 6',
     answers: [
-      { label: 'Answer 1', name: 'six' },
-      { label: 'Answer 2', name: 'six' },
-      { label: 'Answer 3', name: 'six' },
-      { label: 'Answer 4', name: 'six' }
+      { label: 'Answer 1', name: '6' },
+      { label: 'Answer 2', name: '6' },
+      { label: 'Answer 3', name: '6' },
+      { label: 'Answer 4', name: '6' }
     ]
   },
   {
-    prompt: 'Question 7',
+    prompt: 'This is the prompt for question 7',
     answers: [
-      { label: 'Answer 1', name: 'seven' },
-      { label: 'Answer 2', name: 'seven' },
-      { label: 'Answer 3', name: 'seven' },
-      { label: 'Answer 4', name: 'seven' }
+      { label: 'Answer 1', name: '7' },
+      { label: 'Answer 2', name: '7' },
+      { label: 'Answer 3', name: '7' },
+      { label: 'Answer 4', name: '7' }
     ]
   },
   {
-    prompt: 'Question 8',
+    prompt: 'This is the prompt for question 8',
     answers: [
-      { label: 'Answer 1', name: 'eight' },
-      { label: 'Answer 2', name: 'eight' },
-      { label: 'Answer 3', name: 'eight' },
-      { label: 'Answer 4', name: 'eight' }
+      { label: 'Answer 1', name: '8' },
+      { label: 'Answer 2', name: '8' },
+      { label: 'Answer 3', name: '8' },
+      { label: 'Answer 4', name: '8' }
     ]
   },
   {
-    prompt: 'Question 9',
+    prompt: 'This is the prompt for question 9',
     answers: [
-      { label: 'Answer 1', name: 'nine' },
-      { label: 'Answer 2', name: 'nine' },
-      { label: 'Answer 3', name: 'nine' },
-      { label: 'Answer 4', name: 'nine' }
+      { label: 'Answer 1', name: '9' },
+      { label: 'Answer 2', name: '9' },
+      { label: 'Answer 3', name: '9' },
+      { label: 'Answer 4', name: '9' }
     ]
   },
   {
-    prompt: 'Question 10',
+    prompt: 'This is the prompt for question 10',
     answers: [
-      { label: 'Answer 1', name: 'ten' },
-      { label: 'Answer 2', name: 'ten' },
-      { label: 'Answer 3', name: 'ten' },
-      { label: 'Answer 4', name: 'ten' }
+      { label: 'Answer 1', name: '10' },
+      { label: 'Answer 2', name: '10' },
+      { label: 'Answer 3', name: '10' },
+      { label: 'Answer 4', name: '10' }
     ]
   }
 ]
