75200dfd24897cece005560cc5f3d5de0c404045
Jordan Kicklighter <jwkicklighter@gmail.com>
2018-02-25 16:32:56 -0500

    Add sort helper to util library

--- app/js/utils.js
+++ app/js/utils.js
@@ -1,3 +1,4 @@
+// Creates a DOM node from a given string of HTML
 function htmlToElement (html) {
   var template = document.createElement('template')
   html = html.trim()
@@ -5,6 +6,8 @@ function htmlToElement (html) {
   return template.content.firstChild
 }
 
+// Parses a template string to replace values with those supplied in `values` object
+// Emulates the basic functionality of Handlebars
 function replaceTemplateStrings (template, values) {
   return Object.keys(values)
     .map(key => {
@@ -17,13 +20,20 @@ function replaceTemplateStrings (template, values) {
     }, template)
 }
 
+// Combines the above functions into a single one that parses a template string and
+// returns a DOM node ready to be added to the document
 function buildTemplate (template, values) {
   const readyTemplate = values ? replaceTemplateStrings(template, values) : template
   return htmlToElement(readyTemplate)
 }
 
+function sortObjectValues (obj) {
+  return Object.keys(obj).sort((x, y) => obj[x] - obj[y])
+}
+
 module.exports = {
   buildTemplate,
   htmlToElement,
-  replaceTemplateStrings
+  replaceTemplateStrings,
+  sortObjectValues
 }
