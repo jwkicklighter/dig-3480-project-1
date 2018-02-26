776eecbb73c6ac90e232ce62473386c43be3cc8f
Jordan Kicklighter <jwkicklighter@gmail.com>
2018-02-25 21:53:32 -0500

    Add real questions (except for 10 😞)

--- app/js/questions.js
+++ app/js/questions.js
@@ -3,7 +3,7 @@ module.exports = {
     { title: 'Darth Vader', description: 'Sed iaculis ultricies nulla in.' },
     { title: 'Yoda', description: 'Sed iaculis ultricies nulla in.' },
     { title: 'Luke Skywalker', description: 'Sed iaculis ultricies nulla in.' },
-    { title: 'Mace Windu', description: 'Sed iaculis ultricies nulla in.' }
+    { title: 'Han Solo', description: 'Sed iaculis ultricies nulla in.' }
   ],
   questions: [
     {
@@ -12,89 +12,89 @@ module.exports = {
         { label: 'Red', name: '0', bucket: '0' },
         { label: 'Green', name: '0', bucket: '1' },
         { label: 'Blue', name: '0', bucket: '2' },
-        { label: 'Purple', name: '0', bucket: '3' }
+        { label: 'Black', name: '0', bucket: '3' }
       ]
     },
     {
-      prompt: 'This is the prompt for question 2',
+      prompt: 'What trait do you think is the most important in a best friend?',
       answers: [
-        { label: 'Bucket 1', name: '1', bucket: '0' },
-        { label: 'Bucket 2', name: '1', bucket: '1' },
-        { label: 'Bucket 3', name: '1', bucket: '2' },
-        { label: 'Bucket 4', name: '1', bucket: '3' }
+        { label: 'Wisdom', name: '1', bucket: '2' },
+        { label: 'Has your back in a fight', name: '1', bucket: '3' },
+        { label: 'I don\'t need a best friend', name: '1', bucket: '0' },
+        { label: 'I don\'t have friends, I have pupils', name: '1', bucket: '1' }
+      ]
+    },
+    {
+      prompt: 'What is your dream job?',
+      answers: [
+        { label: 'A teacher', name: '1', bucket: '1' },
+        { label: 'A dictator', name: '1', bucket: '0' },
+        { label: 'A politician', name: '1', bucket: '3' },
+        { label: 'Wherever my circumstances take me', name: '1', bucket: '2' }
+      ]
+    },
+    {
+      prompt: 'What is your best quality?',
+      answers: [
+        { label: 'Fighting ability', name: '1', bucket: '3' },
+        { label: 'Wisdom', name: '1', bucket: '1' },
+        { label: 'Luck', name: '1', bucket: '2' },
+        { label: 'Persuasion', name: '1', bucket: '0' }
+      ]
+    },
+    {
+      prompt: 'What would you consider your main weakness?',
+      answers: [
+        { label: 'Your ego gets in the way of doing what\'s right', name: '3', bucket: '3' },
+        { label: 'You let things pass without getting involved', name: '3', bucket: '1' },
+        { label: 'Focusing on one issue blinds you from the big picture', name: '3', bucket: '0' },
+        { label: 'You lack confidence in your own ability', name: '3', bucket: '2' }
+      ]
+    },
+    {
+      prompt: 'How do you go about solving difficult problems?',
+      answers: [
+        { label: 'Diplomatically', name: '4', bucket: '1' },
+        { label: 'Blow it up', name: '4', bucket: '3' },
+        { label: 'With the help of my friends!', name: '4', bucket: '2' },
+        { label: 'Intimidation', name: '4', bucket: '0' }
+      ]
+    },
+    {
+      prompt: 'What is your favorite meal?',
+      answers: [
+        { label: 'Blue milk with sand critters', name: '5', bucket: '2' },
+        { label: 'Steak, well done', name: '5', bucket: '0' },
+        { label: 'Whatever I find in a lost stranger\'s provisions', name: '5', bucket: '1' },
+        { label: 'Anything that comes with a drink', name: '5', bucket: '3' }
+      ]
+    },
+    {
+      prompt: 'What quality do you look for in a significant other?',
+      answers: [
+        { label: 'Someone who needs saving', name: '6', bucket: '3' },
+        { label: 'I am fine being alone', name: '6', bucket: '2' },
+        { label: 'Someone who can look past my flaws', name: '6', bucket: '0' },
+        { label: 'Preferably anyone I\'m not related to...', name: '6', bucket: '3' }
+      ]
+    },
+    {
+      prompt: 'What is your favorite way to keep in touch with your friends?',
+      answers: [
+        { label: 'WhatsApp', name: '7', bucket: '3' },
+        { label: 'Facebook Messenger', name: '7', bucket: '2' },
+        { label: 'Snail Mail', name: '7', bucket: '1' },
+        { label: 'I don\'t have friends', name: '7', bucket: '0' }
+      ]
+    },
+    {
+      prompt: 'This is the prompt for question 10',
+      answers: [
+        { label: 'Answer 1', name: '10', bucket: '0' },
+        { label: 'Answer 2', name: '11', bucket: '0' },
+        { label: 'Answer 3', name: '12', bucket: '0' },
+        { label: 'Answer 4', name: '13', bucket: '0' }
       ]
     }
-    // {
-    //   prompt: 'This is the prompt for question 3',
-    //   answers: [
-    //     { label: 'Answer 1', name: '3', bucket: '0' },
-    //     { label: 'Answer 2', name: '3', bucket: '1' },
-    //     { label: 'Answer 3', name: '3', bucket: '2' },
-    //     { label: 'Answer 4', name: '3', bucket: '3' }
-    //   ]
-    // },
-    // {
-    //   prompt: 'This is the prompt for question 4',
-    //   answers: [
-    //     { label: 'Answer 1', name: '4', bucket: '0' },
-    //     { label: 'Answer 2', name: '4', bucket: '1' },
-    //     { label: 'Answer 3', name: '4', bucket: '2' },
-    //     { label: 'Answer 4', name: '4', bucket: '3' }
-    //   ]
-    // },
-    // {
-    //   prompt: 'This is the prompt for question 5',
-    //   answers: [
-    //     { label: 'Answer 1', name: '5', bucket: '0' },
-    //     { label: 'Answer 2', name: '5', bucket: '1' },
-    //     { label: 'Answer 3', name: '5', bucket: '2' },
-    //     { label: 'Answer 4', name: '5', bucket: '3' }
-    //   ]
-    // },
-    // {
-    //   prompt: 'This is the prompt for question 6',
-    //   answers: [
-    //     { label: 'Answer 1', name: '6', bucket: '0' },
-    //     { label: 'Answer 2', name: '6', bucket: '1' },
-    //     { label: 'Answer 3', name: '6', bucket: '2' },
-    //     { label: 'Answer 4', name: '6', bucket: '3' }
-    //   ]
-    // },
-    // {
-    //   prompt: 'This is the prompt for question 7',
-    //   answers: [
-    //     { label: 'Answer 1', name: '7', bucket: '0' },
-    //     { label: 'Answer 2', name: '7', bucket: '1' },
-    //     { label: 'Answer 3', name: '7', bucket: '2' },
-    //     { label: 'Answer 4', name: '7', bucket: '3' }
-    //   ]
-    // },
-    // {
-    //   prompt: 'This is the prompt for question 8',
-    //   answers: [
-    //     { label: 'Answer 1', name: '8', bucket: '0' },
-    //     { label: 'Answer 2', name: '8', bucket: '1' },
-    //     { label: 'Answer 3', name: '8', bucket: '2' },
-    //     { label: 'Answer 4', name: '8', bucket: '3' }
-    //   ]
-    // },
-    // {
-    //   prompt: 'This is the prompt for question 9',
-    //   answers: [
-    //     { label: 'Answer 1', name: '9', bucket: '0' },
-    //     { label: 'Answer 2', name: '9', bucket: '1' },
-    //     { label: 'Answer 3', name: '9', bucket: '2' },
-    //     { label: 'Answer 4', name: '9', bucket: '3' }
-    //   ]
-    // },
-    // {
-    //   prompt: 'This is the prompt for question 10',
-    //   answers: [
-    //     { label: 'Answer 1', name: '10', bucket: '0' },
-    //     { label: 'Answer 2', name: '11', bucket: '0' },
-    //     { label: 'Answer 3', name: '12', bucket: '0' },
-    //     { label: 'Answer 4', name: '13', bucket: '0' }
-    //   ]
-    // }
   ]
 }
